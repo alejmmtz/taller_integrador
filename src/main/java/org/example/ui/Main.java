c1d70540650ec9150ef80fdf2707e45d745ff84b
Alejandro <alejomum@gmail.com>
2026-08-19 08:43:49 -0500

    feat: "funcion dar salida a vehiculos"

--- src/main/java/org/example/ui/Main.java
+++ src/main/java/org/example/ui/Main.java
@@ -11,26 +11,24 @@ import org.example.exceptions.EspacioOcupadoException;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // TODO: Instanciar ParqueaderoController y cargar datos previos
         ParqueaderoController controller = new ParqueaderoController();
         boolean salir = false;
 
         while (!salir) {
             System.out.println("\n--- SISTEMA DE PARQUEADERO ---");
             System.out.println("1. Registrar Vehículo");
-            System.out.println("2. Listar Vehículos Parqueados");
-            System.out.println("3. Ver Bitácora de Movimientos");
-            System.out.println("4. Guardar y Salir \n");
+            System.out.println("2. Dar Salida a Vehículo");
+            System.out.println("3. Listar Vehículos Parqueados");
+            System.out.println("4. Ver Bitácora de Movimientos");
+            System.out.println("5. Guardar y Salir \n");
             System.out.print("Seleccione una opción \n");
 
-            // TODO: Implementar try-catch para manejar las opciones y entradas del usuario
             try {
                 int opcion = scanner.nextInt();
                 scanner.nextLine();
 
                 switch (opcion) {
                     case 1:
-                        // TODO: Pedir datos, validar y registrar vehículo
                         System.out.println("Registro de datos del Vehiculo \n");
 
                         System.out.print("Placa del vehiculo: ");
@@ -59,17 +57,28 @@ public class Main {
                         } catch (IllegalArgumentException | EspacioOcupadoException e) {
                             System.out.println("ERROR -> ( " + e.getMessage() + " )");
                         }
+
                         break;
 
                     case 2:
-                        System.out.println(controller.mostrarVehiculos());
+                        System.out.println("Ingresa Placa del Vehiculo a dar Salida \n");
+
+                        System.out.print("Placa del vehiculo: ");
+                        String placaSalida = scanner.nextLine();
+
+                        System.out.println(controller.salidaVehiculo(placaSalida));
+
                         break;
 
                     case 3:
-                        Bitacora.mostrarBitacora();
+                        System.out.println(controller.mostrarVehiculos());
                         break;
 
                     case 4:
+                        Bitacora.mostrarBitacora();
+                        break;
+
+                    case 5:
                         salir = true;
                         controller.guardarEstado();
                         System.out.println("Sesión cerrada.");
